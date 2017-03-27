a7ac326753d1f40cec9625ecf59fa9db55fbdf80
niczealind <nickzpd@gmail.com>
2017-03-27 15:23:40 -0700

    Add files via upload

---
+++ commands/forcegrip.py
@@ -0,0 +1,39 @@
+import random
+
+def get_roll(inp):
+    top = int(inp)
+    randomNum = random.randint(1, top)
+    return int(randomNum)
+
+def get_message(name):
+    top = int(5)
+    result = get_roll(top)
+    #return_string = ''
+    #return_string += 'Rolling 1 - %s \n' % top
+    #return_string += 'Result: %s' % result
+
+    if (result == 1):
+        return_string += "%s is picked up with Force Grip and tossed the edge!" % (name)
+
+    if (result == 2):
+        return_string += "%s is picked up with Force Grip, but they are able to break out!" % (name)
+
+    if (result == 3):
+        return_string += "%s is picked up with Force Grip and then released!" % (name)
+
+    if (result == 4):
+        return_string += "%s is picked up with Force Grip, BunkoJuice comes to their rescue!" % (name)
+
+    if (result == 5):
+        return_string += "Force Grip fails on %s!" % (name)
+    
+    return return_string
+    
+async def forcegrip(client, message):
+    try:
+        i = str(message.content).split(' ')
+        name = i[1]
+        await client.send_message(message.channel, get_message(name))
+    except Exception as e:
+        print(e)
+        await client.send_message(message.channel, "ERROR. Not a valid target")
