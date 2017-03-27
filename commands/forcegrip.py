519f95d623f52a3daeab4de4e30582ef1972dc3c
niczealind <nickzpd@gmail.com>
2017-03-27 16:33:49 -0700

    Update forcegrip.py

--- commands/forcegrip.py
+++ commands/forcegrip.py
@@ -6,12 +6,12 @@ def get_roll(inp):
     return int(randomNum)
 
 def get_message(name):
-    top = int(5)
+    top = int(10)
     result = get_roll(top)
     return_string = ''
 
     if (result == 1):
-        return_string = "%s is picked up with Force Grip and tossed the edge!" % (name)
+        return_string = "%s is picked up with Force Grip and tossed off the edge!" % (name)
 
     if (result == 2):
         return_string = "%s is picked up with Force Grip, but they are able to break out!" % (name)
@@ -24,6 +24,21 @@ def get_message(name):
 
     if (result == 5):
         return_string = "Force Grip fails on %s!" % (name)
+        
+    if (result == 6):
+        return_string = "%s is picked up with Force Grip, they use their Force Push to break out!" % (name)
+        
+    if (result == 7):
+        return_string = "%s is tossed into the air with Force Grip then hit with Lightning!" % (name)
+        
+    if (result == 8):
+        return_string = "Force Grip fails on %s, is hit with Force Lighting instead!" % (name)
+        
+    if (result == 9):
+        return_string = "%s is Force Pulled into range then Force Gripped!" % (name)
+        
+    if (result == 10):
+        return_string = "%s is picked up with Force Grip, Force Drain proceeds to absord their energy!" % (name)
     
     return return_string
     
